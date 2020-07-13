16aee745e26dd3f12b356d0688ca1fbdf15c88ea
Johannes Blaschke <jpblaschke@lbl.gov>
2020-07-13 17:21:03 -0400

    expand instrumentation set

--- Scale-Equ-Lightning/run_model.py
+++ Scale-Equ-Lightning/run_model.py
@@ -9,6 +9,11 @@ import pytorch_lightning as pl
 #os.environ["CUDA_VISIBLE_DEVICES"]="0,1,2,3,4,5,6,7"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
+timemory.settings.flat_profile = True
+timemory.settings.timeline_profile = False
+
+
 model = Scale_ResNet()
 trainer = pl.Trainer(gpus=6, max_epochs=1, distributed_backend='ddp')
 trainer.fit(model)
